199e886721e18608fa77bf4ed2a50d3c9862e7c7
Hiroki <1127785+azulamb@users.noreply.github.com>
2022-08-06 20:00:23 +0900

    Prepare unregister SW.

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,28 @@ customElements.whenDefined('ango-contents.ts').then(() => {
 
 if (location.protocol === 'https:' && 'serviceWorker' in navigator) {
 	navigator.serviceWorker.register('./sw.js');
+
+	async function unregister() {
+		await navigator.serviceWorker.getRegistrations().then((registrations) => {
+			let count = 0;
+			for(const registration of registrations) {
+				registration.unregister();
+				++count;
+			}
+			console.log(`Unregister ServiceWorker(${count})`);
+		});
+		await caches.keys().then(function(keys) {
+			return Promise.all(keys.map((cacheName) => {
+				if (cacheName) {
+					console.log(`Delete cache: ${cacheName}`);
+					return caches.delete(cacheName);
+				}
+				return Promise.resolve();
+			})).then(() =>{
+				console.log('Delete caches complete!');
+			}).catch((error) => {
+				console.error(error);
+			});
+		});
+	}
 }
